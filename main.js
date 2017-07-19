8b3db10ed3b8d0f1f0a2e2be260d8ef7b16c7836
Divya Rana <divyarana476@gmail.com>
2017-07-20 00:49:47 +0530

    welcome message displayed using angular directives

---
+++ main.js
@@ -0,0 +1,7 @@
+
+
+	   //$('#user-email').on('input',function() {
+	  // var email = $('#user-email').val()
+	  // var message = 'Welcome Back, ' + email;
+	  // $('.welcome-message').text(message);
+     //});
