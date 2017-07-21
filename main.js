9d31857b01e7fb2f2e2013c6a7b219dca9a31827
Divya Rana <divyarana476@gmail.com>
2017-07-21 11:19:48 +0530

    controller added

--- main.js
+++ main.js
@@ -5,3 +5,12 @@
 	  // var message = 'Welcome Back, ' + email;
 	  // $('.welcome-message').text(message);
      //});
+	 
+	 
+	 var foodieApp = angular.module('foodieApp',[]);
+	 
+	 
+	 foodieApp.controller('mainController',function($scope) {
+     $scope.restaurants = ['Farzi Cafe','Pizza Hut','Wenger\'s Deli','Sagar Ratna'];
+	 
+	 })
