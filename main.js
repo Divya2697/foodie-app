52331b5009c4af90c331b86f625d66de1320fada
Divya Rana <divyarana476@gmail.com>
2017-07-26 00:43:27 +0530

    use kiya aur ingredients found kiya aur link bnaya

--- main.js
+++ main.js
@@ -25,14 +25,16 @@
 })
 
 
-	foodieApp.controller('restaurantController',function($scope,$routeParams) {
+	foodieApp.controller('restaurantController',function($scope,$routeParams,$http) {
 $scope.restaurantId = $routeParams.id;
+$scope.ingredients = [];
 var restaurants = [{
 	name: 'Farzi Cafe',
 	address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
 	location: 'Connaught Place',
 	category: 'Casual Dining, Bar',
 	vote: '4.2',
+	id : 1,
 	cuisines: 'Modern Indian',
 	cost: '2200',
 	hours: '12 Noon to 1 AM (Mon-Sun)',
@@ -44,9 +46,14 @@ var restaurants = [{
 	location: 'Sector 26, Chandigarh',
 	category: 'Casual Dining',
 	vote: '4.6',
+		id : 2,
 	cuisines: 'North Indian, Chinese',
 	cost: '1300',
 	hours: '12 Noon to 3 PM, 6:30 PM to 11 PM (Mon-Sun) ',
+	bestDish: {
+	name: 'Corn Pizza',
+	image: 'http://noblepig.com/images/2016/06/Avocado-and-Three-Bean-Salad-is-perfect-for-a-summertime-barbecue-side-dish.JPG'
+},
 	image: 'https://b.zmtcdn.com/data/pictures/chains/4/120014/481a8438b57d25815e87ee14c75aa3e5.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A'
 },
 {
@@ -55,6 +62,7 @@ var restaurants = [{
 		location: 'Sector 7, Chandigarh',
 		category: 'Fine Dining',
 		vote: '4.3',
+			id : 3,
 		cuisines: 'Italian',
 		cost: '2200',
 		hours: '11:30 AM to 11:30 PM (Mon-Sun) ',
@@ -66,6 +74,7 @@ var restaurants = [{
 		location: 'Sector 8, Chandigarh',
 
 		vote: '4.2',
+			id : 4,
 		cuisines: 'American,Beverages,Desserts',
 		cost: '600',
 		hours: '10 AM to 10:30 PM (Mon-Fri),10 AM to 11 PM (Sat-Sun)',
@@ -77,6 +86,7 @@ var restaurants = [{
 		location: 'Sector 8, Chandigarh',
 		category: 'Quick Bites',
 		vote: '4.5',
+			id : 5,
 		cuisines: 'Burger, Fast Food, Beverages',
 		cost: '500',
 		hours: '12 Noon to 11 PM (Mon-Sun) ',
@@ -89,13 +99,48 @@ var restaurants = [{
 		location: 'Elante Mall, Chandigarh Industrial',
 		category: 'Casual Dining',
 		vote: '4.0',
+			id : 6,
 		cuisines: 'North Indian, Continental, Asian',
 		cost: '1200',
 		hours: '11 AM to 4 PM, 6:30 PM to 11 PM (Mon-Sun) ',
 		image: 'https://b.zmtcdn.com/data/reviews_photos/6b3/43cf511633ce2a717b1d0be23d23e6b3_1414519778.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A'
-	}
-]
+	}]
 	$scope.restaurant = restaurants[$routeParams.id - 1];
+	$scope.getIngredients = function(url) {
+// Do something
+var data = '{"inputs":[{"data":{"image":{"url":"' + url + '"}}}]}'
+$http({
+		'method': 'POST',
+		'url': 'https://api.clarifai.com/v2/models/bd367be194cf45149e75f01d59f77ba7/outputs',
+		'headers': {
+				'Authorization': 'Key f5534695ec9d4ed6a0a90a974e637dfd',
+				'Content-Type': 'application/json'
+		},
+
+
+// 		success: function (response) {
+// 				var ingredients = response.outputs[0].data.concepts;
+// 				var list = '';
+// 				for (var i =0;i < ingredients.length;i++) {
+// 						list += '<div class="ingredient">' + ingredients[i].name + '</div>'
+// 				}
+// 				$('.ingredients').html(list);
+// 		},
+// error: function (xhr) {
+// 				console.log(xhr);
+// 		}
+// })
+'data': data
+}).then(function (response) {
+	var ingredients = response.data.outputs[0].data.concepts;
+	for (var i =0;i < ingredients.length;i++) {
+	$scope.ingredients.push(ingredients[i].name);
+	}
+		}, function (xhr) {
+				console.log(xhr);
+			})
+
+}
 	})
 
 
@@ -115,6 +160,7 @@ var restaurants = [{
 				location: 'Connaught Place',
 				category: 'Casual Dining, Bar',
 				vote: '4.2',
+					id : 1,
 				cuisines: 'Modern Indian',
 				cost: '2200',
 				hours: '12 Noon to 1 AM (Mon-Sun)',
@@ -126,6 +172,7 @@ var restaurants = [{
 				location: 'Sector 26, Chandigarh',
 				category: 'Casual Dining',
 				vote: '4.6',
+					id : 2,
 				cuisines: 'North Indian, Chinese',
 				cost: '1300',
 				hours: '12 Noon to 3 PM, 6:30 PM to 11 PM (Mon-Sun) ',
@@ -137,6 +184,7 @@ var restaurants = [{
 					location: 'Sector 7, Chandigarh',
 					category: 'Fine Dining',
 					vote: '4.3',
+						id : 3,
 					cuisines: 'Italian',
 					cost: '2200',
 					hours: '11:30 AM to 11:30 PM (Mon-Sun) ',
@@ -148,6 +196,7 @@ var restaurants = [{
 					location: 'Sector 8, Chandigarh',
 
 					vote: '4.2',
+						id : 4,
 					cuisines: 'American,Beverages,Desserts',
 					cost: '600',
 					hours: '10 AM to 10:30 PM (Mon-Fri),10 AM to 11 PM (Sat-Sun)',
@@ -159,6 +208,7 @@ var restaurants = [{
 					location: 'Sector 8, Chandigarh',
 					category: 'Quick Bites',
 					vote: '4.5',
+						id : 5,
 					cuisines: 'Burger, Fast Food, Beverages',
 					cost: '500',
 					hours: '12 Noon to 11 PM (Mon-Sun) ',
@@ -171,6 +221,7 @@ var restaurants = [{
 					location: 'Elante Mall, Chandigarh Industrial',
 					category: 'Casual Dining',
 					vote: '4.0',
+						id : 6,
 					cuisines: 'North Indian, Continental, Asian',
 					cost: '1200',
 					hours: '11 AM to 4 PM, 6:30 PM to 11 PM (Mon-Sun) ',
